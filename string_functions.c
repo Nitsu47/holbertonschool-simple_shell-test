b3a0b5bf7c2149a2d37da18e298d2ea4df733d1d
Diego29012 <dieperey598@gmail.com>
2023-08-10 23:58:26 -0700

    update functions

--- string_functions.c
+++ string_functions.c
@@ -29,3 +29,24 @@ void print_env(void)
 	printf("%s\n", *env++);
 	}
 }
+
+
+/**
+* _strcmp - Function compares two strings
+* @s1: first string
+* @s2: second string
+* Return: 1 if strings are the same, 0 if not
+**/
+
+int _strcmp(char *s1, char *s2)
+{
+		unsigned int i = 0;
+
+		while (s1[i])
+		{
+		if (s1[i] != s2[i])
+			return (0);
+			i++;
+		}
+		return (1);
+}
