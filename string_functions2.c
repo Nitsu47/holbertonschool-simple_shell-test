d549c84711790866a4e64bcde14e95acce65ad16
Diego29012 <dieperey598@gmail.com>
2023-08-14 14:50:51 -0700

    add PATH function

---
+++ string_functions2.c
@@ -0,0 +1,30 @@
+#include "shell.h"
+
+/**
+* _getPATH - A function to gets the full value from
+* enviromental variable PATH
+* @env: The pointer to enviromental variables
+* Return: All tokenized pathways for commands
+**/
+
+
+char **_getPATH(char **env)
+{
+		char *pathvalue = NULL, **pathways = NULL;
+		unsigned int i = 0;
+
+		pathvalue = strtok(env[i], "=");
+		while (env[i])
+		{
+		if (_strcmp(pathvalue, "PATH"))
+		{
+		pathvalue = strtok(NULL, "\n");
+		pathways = tokenize(pathvalue, ":");
+		return (pathways);
+		}
+		i++;
+		pathvalue = strtok(env[i], "=");
+		}
+		return (NULL);
+}
+
